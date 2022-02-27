5be3d43561e4000571a52965068bf3bd5f7d353e
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-02-28 02:48:06 +0600

    player added

--- player.js
+++ player.js
@@ -3,7 +3,37 @@ const buttonWarning = () => {
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputValue}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => playerDisplay(data.player))
 
 
+}
+const playerDisplay = players => {
+    for (const player of players) {
+        console.log(player);
+
+
+        const main = document.getElementById('players-div')
+        const div = document.createElement('div')
+
+        div.innerHTML = `  <div class="card" style="width: 18rem;">
+        <img class="card-img-top w-50" src="${player.strThumb}"  alt="...">
+        <div class="card-body">
+        <h3>name:${player.strPlayer}</h3>
+        <h>country:${player.strNationality}</h4>
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" onclick="details('${player.idPlayer}')" class="btn btn-primary">Details</a>
+        </div>
+      </div>
+ 
+ `;
+        main.appendChild(div)
+
+
+    }
+
+}
+
+const details = (info) => {
+    console.log(info);
 }
